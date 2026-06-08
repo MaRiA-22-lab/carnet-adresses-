97d0e60b43374807d808c172447cc3328cc9f06d
MARIA <mariaelmrabet555@gmail.com>
2026-06-08 16:04:14 +0200

    CD-11: feat: ajout commentaire classe Contact

--- v_0.0.1/contact.py
+++ v_0.0.1/contact.py
@@ -13,3 +13,4 @@ class Contact:
         self.telephone = telephone
     def __str__(self):
         return f"{self.nom},{self.email},{self.telephone}"
+# CD-11: Classe Contact - Module Console v0.0.1
